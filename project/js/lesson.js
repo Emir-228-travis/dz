23745cff30b8a2da7d1a918be1b87027613d2fa2
Emir-228-travis <emir.beksultanov14@gmail.com>
2024-11-23 19:05:32 +0600

    Add files via upload

--- project/js/lesson.js
+++ project/js/lesson.js
@@ -62,35 +62,6 @@ const switchTab = () => {
 
 setInterval(switchTab, 3000)
 
-const modal = document.querySelector('.modal')
-const triggerButton = document.querySelector('#btn-get')
-const closeButton = document.querySelector('.modal_close')
-const openModal = () => {
-    modal.style.display = 'block'
-    document.body.style.overflow = 'hidden'
-}
-const closeModal = () => {
-    modal.style.display = 'none'
-    document.body.style.overflow = ''
-}
-triggerButton.onclick = () => openModal();
-closeButton.onclick = () => closeModal();
-modal.onclick = (event) => {
-    if (event.target === modal) {
-        closeModal()
-    }
-};
-const showModalOnScroll = () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        openModal();
-        window.removeEventListener('scroll', showModalOnScroll);
-    }
-};
-window.addEventListener('scroll', showModalOnScroll);
-const showModalAfterDelay = () => {
-    setTimeout(openModal, 10000);
-};
-window.addEventListener('load', showModalAfterDelay);
 
 //converter
 
--- project/js/modal.js
+++ project/js/modal.js
@@ -1,15 +1,35 @@
+//MODAL
 const modal = document.querySelector('.modal')
 const triggerButton = document.querySelector('.modal__trigger')
 const closeButton = document.querySelector('.modal__close')
 
-const openModal = () => {
+    const openModal = () => {
     modal.style.display = 'block'
-    d
+    document.body.style.overflow = 'hidden'
 }
 
 const closeModal = () => {
     modal.style.display = 'none'
+    document.body.style.overflow = ''
 }
 
 triggerButton.onclick = () => openModal()
 closeButton.onclick = () => closeModal()
+
+
+
+modal.onclick = (event) => {
+    if (event.target === modal) {
+        closeModal()
+    }
+};
+const showModalOnScroll = () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        openModal();
+        window.removeEventListener('scroll', showModalOnScroll);
+    }
+};
+window.addEventListener('scroll', showModalOnScroll);
+const showModalAfterDelay = () => {
+    setTimeout(openModal, 10000);}
+window.addEventListener('load', showModalAfterDelay);
